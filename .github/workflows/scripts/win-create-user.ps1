5a9f3098cff497736d2b2709bccd8de3fd95429d
MikeZhang69 <hongyanzhang69@outlook.com>
2025-12-06 23:48:28 +0800

    feat: support fixed RDP password via RDP_PASSWORD secret

--- .github/workflows/scripts/win-create-user.ps1
+++ .github/workflows/scripts/win-create-user.ps1
@@ -1,17 +1,25 @@
-# Create RDP user with strong password
-Add-Type -AssemblyName System.Security
-$charSet = @{
-  Upper   = [char[]](65..90)
-  Lower   = [char[]](97..122)
-  Number  = [char[]](48..57)
-  Special = ([char[]](33..47) + [char[]](58..64) + [char[]](91..96) + [char[]](123..126))
+# Create RDP user with password from secret or generate random
+# To use a fixed password, set RDP_PASSWORD in GitHub Secrets
+
+if ($env:RDP_PASSWORD) {
+    Write-Host "Using password from RDP_PASSWORD secret"
+    $password = $env:RDP_PASSWORD
+} else {
+    Write-Host "Generating random password (set RDP_PASSWORD secret for fixed password)"
+    Add-Type -AssemblyName System.Security
+    $charSet = @{
+      Upper   = [char[]](65..90)
+      Lower   = [char[]](97..122)
+      Number  = [char[]](48..57)
+      Special = ([char[]](33..47) + [char[]](58..64) + [char[]](91..96) + [char[]](123..126))
+    }
+    $rawPassword = @()
+    $rawPassword += $charSet.Upper | Get-Random -Count 4
+    $rawPassword += $charSet.Lower | Get-Random -Count 4
+    $rawPassword += $charSet.Number | Get-Random -Count 4
+    $rawPassword += $charSet.Special | Get-Random -Count 4
+    $password = -join ($rawPassword | Sort-Object { Get-Random })
 }
-$rawPassword = @()
-$rawPassword += $charSet.Upper | Get-Random -Count 4
-$rawPassword += $charSet.Lower | Get-Random -Count 4
-$rawPassword += $charSet.Number | Get-Random -Count 4
-$rawPassword += $charSet.Special | Get-Random -Count 4
-$password = -join ($rawPassword | Sort-Object { Get-Random })
 $securePass = ConvertTo-SecureString $password -AsPlainText -Force
 New-LocalUser -Name "vum" -Password $securePass -AccountNeverExpires
 Add-LocalGroupMember -Group "Administrators" -Member "vum"
